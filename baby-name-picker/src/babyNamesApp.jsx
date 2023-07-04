f6b3c035f95232d66f960d14b30ea614bd20af8b
Saliha Popal <salihapopal@gmail.com>
2023-07-04 22:15:03 +0100

    Level 3

--- baby-name-picker/src/babyNamesApp.jsx
+++ baby-name-picker/src/babyNamesApp.jsx
@@ -35,6 +35,57 @@
 
 
 //   level 2
+// import React, { useState, useEffect } from 'react';
+// import data from './babyNamesData.json';
+// import './App.css';
+
+// const BabyNamesApp = () => {
+//   const [searchTerm, setSearchTerm] = useState('');
+// const [filteredNames, setFilteredNames] = useState([]);
+
+
+// const handleSearch = (e) => {
+//   setSearchTerm(e.target.value);
+// };
+
+// useEffect(() => {
+//   const filtered = data.filter((name) =>  name.name.toLowerCase().includes(searchTerm.toLowerCase())
+//   );
+//   setFilteredNames(filtered);
+// }, [searchTerm]);
+
+// return (
+//   <div className="baby-names-container">
+//     <div className="input">
+//       <input
+//         type="text"
+//         placeholder="Search for a name..."
+//         value={searchTerm}
+//         onChange={handleSearch}
+//       />
+//     </div>
+//     <div className="name-list">
+//       {filteredNames.map((name) => (
+//         <div
+//           key={name.id}
+//           className="name-item"
+//           style={{
+//             backgroundColor: name.sex === 'f' ? 'lightpink' : 'lightblue',
+//           }}
+//         >
+//           {name.name} 
+//         </div>
+//       ))}
+//     </div> 
+//   </div>
+  
+//   );
+// }
+// export default BabyNamesApp;
+
+
+
+// Level 3
 import React, { useState, useEffect } from 'react';
 import data from './babyNamesData.json';
 import './App.css';
@@ -42,6 +93,7 @@ import './App.css';
 const BabyNamesApp = () => {
   const [searchTerm, setSearchTerm] = useState('');
 const [filteredNames, setFilteredNames] = useState([]);
+const [favorite, setFavorites] = useState([]);
 
 
 const handleSearch = (e) => {
@@ -49,11 +101,20 @@ const handleSearch = (e) => {
 };
 
 useEffect(() => {
-  const filtered = data.filter((name) =>
-    name.name.toLowerCase().includes(searchTerm.toLowerCase())
+  const filtered = data.filter((name) =>  name.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
   setFilteredNames(filtered);
 }, [searchTerm]);
+
+const handleAddToFavorites = (name) => {
+  setFavorites([...favorite, name]);
+  setFilteredNames(filteredNames.filter((name) => name.name !== name));
+}
+
+const handleRemoveFavorites = (name) => {
+  setFilteredNames([...favorite, name]);
+  setFavorites(favorite.filter((name) => name.name !== name));
+}
 
 return (
   <div className="baby-names-container">
@@ -65,6 +126,21 @@ return (
         onChange={handleSearch}
       />
     </div>
+    <div className="favourite-name-list"> 
+    <p>Favorite:</p>
+    {favorite.map((name)=>
+    <div
+    key={name.id}
+    className="name-item"
+    style = {{
+      backgroundColor: name.sex === "f" ? "lightpink" : "lightblue",
+    }}
+    onClick={() => handleRemoveFavorites(name)}
+    >
+    {name.name}
+    </div>
+    )}
+  </div>
     <div className="name-list">
       {filteredNames.map((name) => (
         <div
@@ -73,17 +149,14 @@ return (
           style={{
             backgroundColor: name.sex === 'f' ? 'lightpink' : 'lightblue',
           }}
+          onClick={() => handleAddToFavorites(name)}
         >
           {name.name} 
         </div>
       ))}
-    </div> 
+    </div>
   </div>
-  
   );
 }
 export default BabyNamesApp;
-
-
-
 
